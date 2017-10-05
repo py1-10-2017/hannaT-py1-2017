bcfd392d6dd225f44d9dd0794a9886ba1cf29451
seyoumha <seyoumha@gmail.com>
2017-10-05 14:36:20 -0400

    names

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+def self_info():
+    info = {"name": "Hanna", "age": 100, "Country of birth": "The United States", "Favorite language": "Python" }
+    print "My name is " + info['name']
+    print "My age is {}".format(info['age'])
+    print "My country of birth is " + info['Country of birth']
+    print "my favoriate language is " + info['Favorite language']
+
+self_info()
