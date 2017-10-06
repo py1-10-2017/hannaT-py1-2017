6b39f77a7bd64c624779637017298b4a929dfba9
seyoumha <seyoumha@gmail.com>
2017-10-06 13:33:55 -0400

    dictionary in tuples out

---
+++ dictionary_tuples.py
@@ -0,0 +1,10 @@
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def dictionary_in_tuples_out(dict):
+    print dict.items()    
+dictionary_in_tuples_out(my_dict)
+        
